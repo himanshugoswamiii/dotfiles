970168ead8fc53ed8e0f0a21bdff8b721fd608f4
Himanshu Goswami <himanshugoswami.12feb@gmail.com>
2025-01-22 17:01:54 +0530

    neovim: Lazyvim big update (14.x)
    - fzf-lua as replacement for telescope.nvim
    - blink.cmp as replacement for nvim-cmp

---
+++ dot_config/nvim/lua/plugins/blink-cmp.lua
@@ -0,0 +1,9 @@
+return {
+    "saghen/blink.cmp",
+    opts = {
+        keymap = {
+            preset = "super-tab",
+            -- super-tab : uses tab for smart select_and_accept
+        },
+    },
+}
---
+++ dot_config/nvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,9 @@
+return {
+    "ibhagwan/fzf-lua",
+    keys = {
+        -- disabling extra keymaps
+        { "<leader>/", false }, -- grep text (root dir) : i'll use <leader> + s + g
+        { "<leader><space>", false }, -- Find files : i'll use <leader> + f + f
+        { "<leader>,", false }, -- Switch buffer : Use <leader> + f + b
+    },
+}
