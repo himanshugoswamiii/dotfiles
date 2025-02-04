ade1de5ab2658a8faa3d92f39921f6bb99be4a6f
Himanshu Goswami <himanshugoswami.12feb@gmail.com>
2025-02-04 21:43:09 +0530

    neovim: fzf-lua setup for vertical split

--- dot_config/nvim/lua/plugins/fzf-lua.lua
+++ dot_config/nvim/lua/plugins/fzf-lua.lua
@@ -1,5 +1,27 @@
+--[[ Keymaps:
+      ["shift-down"]  = "preview-page-down",
+      ["shift-up"]    = "preview-page-up",
+--]]
 return {
     "ibhagwan/fzf-lua",
+    -- Note: opts must be a table, Lazy.nvim will merge it into require("fzf-lua").setup()
+    --      if you need to run extra Lua code then use config = function()
+    opts = {
+        defaults = {
+            file_icons = false,
+        },
+        winopts = {
+            preview = {
+                layout = "vertical",
+            },
+        },
+        previewers = {
+            builtin = {
+                -- With this change, the previewer will not add syntax highlighting to files larger than 100KB
+                syntax_limit_b = 1024 * 100, -- 100KB
+            },
+        },
+    },
     keys = {
         -- disabling extra keymaps
         { "<leader>/", false }, -- grep text (root dir) : i'll use <leader> + s + g
